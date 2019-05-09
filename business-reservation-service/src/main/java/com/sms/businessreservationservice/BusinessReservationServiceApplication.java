ffc464049d9c8ac92bccf7d6787ce0eb64f0dad2
mathuranchal90 <Anchal.Mathur@BOLD-ANCHALM>
2019-05-09 17:11:06 +0530

    May9

---
+++ business-reservation-service/src/main/java/com/sms/businessreservationservice/BusinessReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sms.businessreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessReservationServiceApplication.class, args);
+	}
+
+}
